94a019951742de25c3c71a057c159d4f0c4828ad
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2022-04-13 14:06:31 -0300

    remove unused images

--- src/utils/mockImages.js
+++ src/utils/mockImages.js
@@ -1,7 +1,1 @@
-// ----------------------------------------------------------------------
-
-export const mockImgCover = (index) => `/static/mock-images/covers/cover_${index}.jpg`;
-export const mockImgProduct = (index) => `/static/mock-images/products/product_${index}.jpg`;
-export const mockImgAvatar = (index) => `/static/mock-images/avatars/avatar_${index}.jpg`;
-
 export const mockImgTea = (teaName) => `/static/mock-images/teas/${teaName}.jpg`;
