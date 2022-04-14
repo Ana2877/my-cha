06421be71e3c93e2f839f4ab1da3822578983a5d
Ana2877 <acpagnoncelli@inf.ufrgs.br>
2022-04-13 22:27:10 -0300

    remove packages

--- src/pages/PreparingTea.js
+++ src/pages/PreparingTea.js
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import PreparingTeaContent from '../sections/PreparingTeaContent';
 import Page from '../components/Page';
 
